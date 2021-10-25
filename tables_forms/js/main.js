04e97dae3ca885dbc05528f838696783be51d0c3
Anthony C <aac.wwt@gmail.com>
2021-10-25 01:06:30 -0500

    all kinds of changes

---
+++ tables_forms/js/main.js
@@ -0,0 +1,10 @@
+/*console.log("!!!Hello World!!!")*/
+
+/*function calculation()
+{
+    var o = document.getElementById("result");
+    var num1 = document.getElementById("num1");
+    var num2 = document.getElementById("num2");
+    
+    o.value = parseFloat(num1.value) + parseFloat(num2.value);
+}*/
